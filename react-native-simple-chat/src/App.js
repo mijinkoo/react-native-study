9bdadf061265cba8bda9187632fbd2c04e9a02b9
mijinkoo <starmj09@naver.com>
2022-01-20 20:54:40 +0900

    9.4.1 내비게이션

--- react-native-simple-chat/src/App.js
+++ react-native-simple-chat/src/App.js
@@ -1,10 +1,11 @@
 import React, {useState} from 'react';
 import {StatusBar, Image} from 'react-native';
-import {AppLoading} from 'expo';
+import AppLoading from 'expo-app-loading';
 import {Asset} from 'expo-asset';
 import * as Font from 'expo-font';
 import {ThemeProvider} from 'styled-components/native';
 import { theme } from './theme';
+import Navigation from './navigations';
 
 const cacheImages = images => {
     return images.map(image => {
@@ -33,6 +34,7 @@ const App = () => {
     return isReady ? (
         <ThemeProvider theme={theme}>
             <StatusBar barStyle="dark-content" />
+            <Navigation />
         </ThemeProvider>
     ) : (
         <AppLoading
