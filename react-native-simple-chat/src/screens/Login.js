981e2bad8ff93be9cf920e5f35e5cc95e4b57018
mijinkoo <starmj09@naver.com>
2022-01-20 23:56:50 +0900

    9.4.2 로그인 화면

--- react-native-simple-chat/src/screens/Login.js
+++ react-native-simple-chat/src/screens/Login.js
@@ -1,20 +1,47 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
 import styled from 'styled-components/native';
-import { Text, Button } from 'react-native';
+import { Text, Button, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { Image, Input } from '../components';
+import { images } from '../utils/images';
 
 const Container = styled.View`
 flex: 1;
 justify-content: center;
 align-items: center;
 background-color: ${({ theme }) => theme.background };
+padding: 20px;
 `;
 
 const Login = ({ navigation }) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const passwordRef = useRef();
+
     return (
-        <Container>
-            <Text style={{fontSize: 30}}>Login Screen</Text>
-            <Button title="Signup" onPress={() => navigation.navigate('Signup')}/>
-        </Container>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <Container>
+                <Image url={images.logo} imageStyle={{borderRadius: 8}}/>
+                <Input 
+                    label='Email'
+                    value={email}
+                    onChangeText={text => setEmail(text)}
+                    onSubmitEditing={() => passwordRef.current.focus()}
+                    placeholder='Email'
+                    returnKeyType='next'
+                />
+                <Input
+                    ref={passwordRef}
+                    label='Password'
+                    value={password}
+                    onChangeText={text => setPassword(text)}
+                    onSubmitEditing={() => {}}
+                    placeholder='Password'
+                    returnKeyType='done'
+                    isPassword
+                />
+                <Text style={{fontSize: 30}}>Login Screen</Text>
+            </Container>
+        </TouchableWithoutFeedback>
     );
 };
 
---
+++ react-native-simple-chat/src/utils/images.js
@@ -0,0 +1,5 @@
+const prefix = 'https://firebasestorage.googleapis.com/v0/b/react-native-simple-chat-ac34f.appspot.com/o';
+
+export const images = {
+    logo: `${prefix}/logo.png?alt=media`,
+}
